04465ce6e9330e660a2c9c4104c576b5f1707128
hasibur4389 <hasibur@braincraftapps.com>
2023-09-20 16:34:21 +0600

    Initial Commit

---
+++ GalleryModule/GalleryModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryModuleApp.swift
+//  GalleryModule
+//
+//  Created by Hasibur Rahman on 20/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GalleryModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
